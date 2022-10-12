7bfa130a30c4f8cf162511227ea0aa416f17ff66
Viraj <virajsankalpa@gmail.com>
2022-10-12 14:06:09 +0530

    added http dependencies

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:sam_api/models/post.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +11,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Posts>? posts;
+  var isLoaded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text("Posts"),
-    ));
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Text("Hi"),
+          );
+        },
+      ),
+    );
   }
 }
